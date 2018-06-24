7e675309a9f42d01f0ff6937c7431fc035169c4c
koushikpaul1 <koushikpaul1@gmail.com>
2018-06-24 21:42:20 +0530

    ere

---
+++ Udemy/Microservices/currency-exchange-service/src/main/java/com/edge/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,28 @@
+package com.edge.microservices.currencyexchangeservice;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+	
+	@Autowired
+	private Environment environment;
+	
+	@Autowired
+	private ExchangeValueRepository exchangeValueRepository;
+	
+	@GetMapping("/currency-exchane/from/{from}/to/{to}")
+	public ExchangeValue retrieveExchangeValue(@PathVariable String from,@PathVariable String to) {
+		ExchangeValue exchangeValue=new ExchangeValue(1000L, from,to,BigDecimal.valueOf(65));
+		exchangeValueRepository.findByFromAndTo(from, to);
+		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		 return exchangeValue;
+		}
+
+}
