65074d79ce0ec66b9de0b9c552e45717d3bd5540
koushikpaul <koushik.paul@leadics.com>
2018-06-25 10:24:29 +0530

    5

--- Udemy/Microservices/currency-exchange-service/src/main/java/com/edge/microservices/currencyexchangeservice/ExchangeValueRepository.java
+++
@@ -1,8 +0,0 @@
-package com.edge.microservices.currencyexchangeservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long>{
-	ExchangeValue findByFromAndTo(String from,String to );
-
-}
