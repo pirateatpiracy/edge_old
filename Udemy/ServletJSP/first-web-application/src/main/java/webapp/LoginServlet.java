3ccd930abee6829a7b1d5e49ad981d997c19bcea
edge <edge@DESKTOP-SB2GJDJ>
2018-07-02 20:52:20 +0530

    2

--- Udemy/ServletJSP/first-web-application/src/main/java/webapp/LoginServlet.java
+++ Udemy/ServletJSP/first-web-application/src/main/java/webapp/LoginServlet.java
@@ -3,6 +3,7 @@ package webapp;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,9 +16,9 @@ import javax.servlet.http.HttpServletResponse;
  * JEE with Servlets
  * 
  * Web Server responds with Http Response
- */
-
-//Java Platform, Enterprise Edition (Java EE) JEE6
+ * 
+ * 
+ * //Java Platform, Enterprise Edition (Java EE) JEE6
 
 //Servlet is a Java programming language class 
 //used to extend the capabilities of servers 
@@ -28,21 +29,14 @@ import javax.servlet.http.HttpServletResponse;
 //2. @WebServlet(urlPatterns = "/login.do")
 //3. doGet(HttpServletRequest request, HttpServletResponse response)
 //4. How is the response created?
+ */
 
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>Yahoo!!!!!!!!</title>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("My First Servlet");
-		out.println("</body>");
-		out.println("</html>");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
 
 	}
 
