ae94b6f368125aa0e6e4ee45b766766fed45e572
koushikpaul1 <koushikpaul1@gmail.com>
2018-07-09 16:31:35 +0530

    DataStructure

---
+++ java/corejava/DataStructure/src/main/java/java/edge/LibraryClasses.java
@@ -0,0 +1,44 @@
+package java.edge;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.Vector;
+
+@SuppressWarnings("rawtypes")
+public class LibraryClasses {
+	
+	
+	
+	
+	List ArrayList=new ArrayList();
+	List vector=new Vector();	
+	List linkedList=new LinkedList();
+	
+	Queue linkedListQueue=new LinkedList();
+	Queue piorityQueue=new PriorityQueue();
+	
+	Set hashSet=new HashSet();
+	Set linkedHashSet=new LinkedHashSet();
+	SortedSet treeSet=new TreeSet();
+	
+	Map hashMap=new HashMap();
+	Map hashTable=new Hashtable();
+	SortedMap treeMap=new TreeMap();
+    public static void main( String[] args ) throws IllegalArgumentException, Exception{
+    	
+    }
+
+}
