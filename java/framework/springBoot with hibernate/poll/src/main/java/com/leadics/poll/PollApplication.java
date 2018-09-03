7ad6629eeea3c1cf05772b342a7cff3e3d1b6680
unknown <koushikpaul1@gmail.com>
2018-09-04 00:31:59 +0530

    spring boot hibernate

---
+++ java/framework/springBoot with hibernate/poll/src/main/java/com/leadics/poll/PollApplication.java
@@ -0,0 +1,12 @@
+package com.leadics.poll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollApplication.class, args);
+	}
+}
