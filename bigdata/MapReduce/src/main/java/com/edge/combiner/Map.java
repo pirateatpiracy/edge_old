98e055086b7257f2fcaf8c5e20f76385a7525572
koushik <koushik@ubuntu>
2018-11-25 12:53:57 +0530

    pom fixes, Mapreduce project working , other projects didnt check

--- bigdata/MapReduce/src/main/java/com/edge/combiner/Map.java
+++ bigdata/MapReduce/src/main/java/com/edge/combiner/Map.java
@@ -14,7 +14,7 @@ public class Map extends Mapper<LongWritable, Text, Text, Average> {
 	
 	protected void map(LongWritable offSet, Text line, Context context)	throws IOException, InterruptedException {
 		row = line.toString().split(" ");
-		System.out.println("Key "+row[0]+"Value "+row[1]);
+		System.out.println("Key> "+row[0]+" Value >"+row[1]);
 		name.set(row[0]);
 		context.write(name, new Average(Integer.parseInt(row[1].toString()), 1));
 	}
